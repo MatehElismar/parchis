326087ed9d051b24dab833546daa0e7ec2ebab6d
Mateh Elismar <matematicoelismar@gmail.com>
2021-02-09 15:09:26 -0400

    movimiento de los turnos y dados

--- index.js
+++ index.js
@@ -13,25 +13,45 @@ dado4.src = "img/dado4.png"
 dado5.src = "img/dado5.png"
 dado6.src = "img/dado6.png"
 
+let turno = -1;
+let turnoColor = 'amarillo'
+
 
 function tirar() {
+    const dado1Imagen = document.getElementById('dado1-imagen')
+    const dado2Imagen = document.getElementById('dado2-imagen')
+    dado1Imagen.classList.remove('hidden')
+    dado2Imagen.classList.remove('hidden')
     let rand = Math.floor((Math.random() * 6) + 1)
     document.images["ima1"].src = eval("dado" + rand + ".src");
-    const dado1Result = document.getElementById('dato1-result');
-    dado1Result.innerHTML = rand;
+    const dados1Result = document.querySelectorAll('.dado1-result');
+    dados1Result.forEach(el => {
+        el.innerHTML = rand;
+
+    })
     rand = Math.floor((Math.random() * 6) + 1)
-    const dado2Result = document.getElementById('dato2-result');
-    dado2Result.innerHTML = rand;
+    const dados2Result = document.querySelectorAll('.dado2-result');
+    dados2Result.forEach(el => {
+        el.innerHTML = rand;
+
+    })
     document.images["ima2"].src = eval("dado" + rand + ".src");
+
+    const fichas = document.querySelectorAll(`.ficha.${turnoColor}`);
+    fichas.forEach(f => {
+        f.classList.add('dropdown')
+    })
 }
 
-function rollTheDice() {
 
+function start() {
+    siguienteTurno();
 }
 
 
 
 function moverFicha(fichaID, steps) {
+    if (steps == 6) steps = 12;//el 6 vale 12
     const ficha = document.getElementById(fichaID);
     let spot = null;
     for (let i = 0; i < steps; i++) {
@@ -68,15 +88,111 @@ function sacarFicha(fichaID) {
     if (ficha.dataset.color == 'rojo') ficha.dataset.spot = 39
     spot = document.getElementById(`spot-${ficha.dataset.spot}`);
     spot.appendChild(ficha)
+    ficha.dataset.active = true;
+}
+
+function siguienteTurno() {
+    const lanzarBtn = document.getElementById('lanzarButton')
+    const dado1Imagen = document.getElementById('dado1-imagen')
+    const dado2Imagen = document.getElementById('dado2-imagen')
+
+    // desabilitar el turno anterior
+    const fichas = document.querySelectorAll(`.ficha.${turnoColor}`);
+    fichas.forEach(f => {
+        f.classList.remove('dropdown')
+    })
+
+    const dados1Result = document.querySelectorAll('.dado1-result');
+    dados1Result.forEach(el => {
+        el.disabled = false;
+    })
+
+    const dados2Result = document.querySelectorAll('.dado2-result');
+    dados2Result.forEach(el => {
+        el.disabled = false;
+    })
+
+    dado1Disabled = false;
+    dado2Disabled = false;
+
+    if (turno < 3) {
+        turno++;
+    }
+    else turno = 0;
+
+    let homeID = ''
+    if (turno == 0) {
+        turnoColor = 'amarillo'
+        homeID = 'amarillo-home'
+    }
+    else if (turno == 1) {
+        turnoColor = 'azul'
+        homeID = 'azul-home'
+    }
+    else if (turno == 2) {
+        turnoColor = 'rojo'
+        homeID = 'rojo-home'
+    }
+    else if (turno == 3) {
+        turnoColor = 'verde'
+        homeID = 'verde-home'
+    }
+    const home = document.getElementById(homeID)
+    lanzarBtn.classList.remove('hidden')
+    dado1Imagen.classList.add('hidden')
+    dado2Imagen.classList.add('hidden')
+    home.appendChild(lanzarBtn)
+    home.appendChild(dado1Imagen)
+    home.appendChild(dado2Imagen)
+}
+
+
+let dado1Disabled = false;
+let dado2Disabled = false;
+function onDiceClick(fichaID, e) {
+    const ficha = document.getElementById(fichaID);
+    const steps = e.target.innerHTML;
+
+
+    if (ficha.dataset.active || steps == 5) {
+        if (e.target.className.includes('dado1-result')) {
+            dado1Disabled = true;
+            const dados1Result = document.querySelectorAll('.dado1-result');
+            dados1Result.forEach(el => {
+                el.disabled = true;
+
+            })
+        }
+        else if (e.target.className.includes('dado2-result')) {
+            dado2Disabled = true;
+            const dados2Result = document.querySelectorAll('.dado2-result');
+            dados2Result.forEach(el => {
+                el.disabled = true;
+
+            })
+        }
+    }
+    if (ficha.dataset.active) {
+        moverFicha(fichaID, steps)
+
+    }
+    else if (steps == 5) {
+        sacarFicha(fichaID)
+    }
+    else alert('nope')
+
+    if (dado1Disabled && dado2Disabled) {
+
+        siguienteTurno();
+    }
+
 }
 
 
 (function () {
     // your page initialization code here
-    // the DOM will be available here
-    const moveButton = document.getElementById('moveButton');
-    const sacarButton = document.getElementById('sacarButton');
-    sacarButton.addEventListener('click', () => sacarFicha("ficha10"))
-    moveButton.addEventListener('click', () => moverFicha('ficha10', 70))
+    // the DOM will be available here 
+    const startButton = document.getElementById('startButton');
+    startButton.addEventListener('click', () => start())
 
 })();
